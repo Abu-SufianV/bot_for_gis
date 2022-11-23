4d15f3620d91367c553bcfcb223fcc449a05cc69
Dr Proger <dr-proger@a-vagabov.ru>
2022-11-24 00:10:41 +0300

    Configuration settings

---
+++ configs/log_config.py
@@ -0,0 +1,8 @@
+import logging
+from datetime import datetime
+
+log_filename = f"logs/log__{datetime.date(datetime.now())}.log".replace("-", "_")
+log_format = '%(asctime)s [%(levelname)s] %(filename)s - %(funcName)s: %(message)s'
+log_level = logging.INFO
+log_encoding = 'UTF-8'
+logging.basicConfig(level=log_level, filename=log_filename, format=log_format, encoding=log_encoding)
